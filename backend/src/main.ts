6207c87fed35726f022441db99ce0a258d792005
svetozar12 <s.gospodinov02@gmail.com>
2024-08-03 17:59:48 +0300

    init projects

---
+++ backend/src/main.ts
@@ -0,0 +1,21 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import { exampleRouter } from "./routes/example/example";
+import { specs } from "./config/swagger";
+
+const app = express();
+
+app.use("/api", exampleRouter);
+app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/v1/swagger/doc.json", (req, res) => res.send(specs));
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on("error", console.error);
---
+++ device_script/src/main/java/com/svetozar12/Main.java
@@ -0,0 +1,7 @@
+package com.svetozar12;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
